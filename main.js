e3537252bd8d1044d2dd031829f7ba98cf964e15
BganaRoxa <89401543+BganaRoxa@users.noreply.github.com>
2023-01-27 00:37:01 -0400

    adicionando aviso de input duplicado

--- main.js
+++ main.js
@@ -1,28 +1,39 @@
 const form = document.getElementById('form-agenda')
+const novoContato = []
+const novoTelefone = []
 let contatos = ''
 
 form.addEventListener('submit', function(e) {
     e.preventDefault()
 
+    adicionaLinha()
+    atualizaTabela()
+})
+
+function adicionaLinha() {
     const nomeContato = document.getElementById('nome-contato')
     const telContato = document.getElementById('telefone-contato')
 
-    let contato = '<tr>'
-    contato += `<td>${nomeContato.value}</td>`
-    contato += `<td>${telContato.value}</td>`
-    contato += `<td><a href="https://api.whatsapp.com/send?phone=55${telContato.value}&text=Ol%C3%A1"><img src="./images/contato.png" alt="Imagem de um telefone" /></td>`
-    contato += `</tr>`
+    if ((novoContato.includes(nomeContato.value))||(novoTelefone.includes(telContato.value))) {
+        alert('O Nome do Contato ou o Número que você está tentando incluir, já foi salvo. Confira e tente novamente.')
+    } else {
+        novoContato.push(nomeContato.value)
+        novoTelefone.push(telContato.value)
 
-    contatos += contato
+        let contato = '<tr>'
+        contato += `<td>${nomeContato.value}</td>`
+        contato += `<td>${telContato.value}</td>`
+        contato += `<td><a href="https://api.whatsapp.com/send?phone=55${telContato.value}&text=Ol%C3%A1"><img src="./images/contato.png" alt="Imagem de um telefone" /></td>`
+        contato += `</tr>`
 
-    const corpoTabela = document.querySelector('tbody')
-    corpoTabela.innerHTML = contatos
+        contatos += contato
+    }
 
     nomeContato.value = ''
     telContato.value = ''
 }
-)
 
-
-
-
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody')
+    corpoTabela.innerHTML = contatos
+}
